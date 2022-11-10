705d2633f317b7d2877d5ceab7fc27a1a8d79849
4118BarbolinS.P <skat.apps@gmail.com>
2022-11-11 00:04:19 +0300

    Реализация функций обновления отзыва и места

--- app/src/main/java/com/example/spacevent/model/database/PlacesDataSource.kt
+++ app/src/main/java/com/example/spacevent/model/database/PlacesDataSource.kt
@@ -20,9 +20,24 @@ object PlacesDataSource {
         return db.collection("places").document(place.id).set(place)
     }
 
+    suspend fun updatePlace(placeId: String, changes: Map<String, Any>): Task<Void> {
+        return db.collection("places").document(placeId).update(changes)
+    }
+
     suspend fun createReview(placeId: String, review: Review): Task<Void> {
         return db.collection("places")
             .document(placeId).collection("reviews")
             .document(review.user).set(review)
     }
+
+    suspend fun updateReview(
+        placeId: String,
+        reviewId: String,
+        changes: Map<String, Any>
+    ): Task<Void> {
+        return db.collection("places")
+            .document(placeId).collection("reviews")
+            .document(reviewId).update(changes)
+    }
+
 }
