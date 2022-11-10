e64698e7494f5f48fdbcfa4dcad64a2f1aff20ca
4118BarbolinS.P <skat.apps@gmail.com>
2022-11-10 12:09:00 +0300

    Подключение firebase к проекту

---
+++ app/src/main/java/com/example/spacevent/model/database/PlacesDataSource.kt
@@ -0,0 +1,7 @@
+package com.example.spacevent.model.database
+
+import com.google.firebase.firestore.FirebaseFirestore
+
+object PlacesDataSource {
+    private val db = FirebaseFirestore.getInstance()
+}
