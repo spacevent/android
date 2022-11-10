a38f449427aeac4f92b157fe87173ef6b6c40ddf
4118BarbolinS.P <skat.apps@gmail.com>
2022-11-11 00:22:08 +0300

    Разделение PlacesViewModel.kt на: * PlacesViewModel.kt * ReviewViewModel.kt

--- app/src/main/java/com/example/spacevent/viewmodel/ReviewViewModel.kt
+++ app/src/main/java/com/example/spacevent/viewmodel/ReviewViewModel.kt
@@ -3,17 +3,17 @@ package com.example.spacevent.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.spacevent.model.database.PlacesDataSource
-import com.example.spacevent.model.emptities.Place
 import com.example.spacevent.model.emptities.Review
 import com.google.firebase.firestore.ListenerRegistration
-import kotlinx.coroutines.launch
 
-class PlacesViewModel : ViewModel() {
-    private val _places by lazy { MutableLiveData<List<Place>>() }
-    val places: LiveData<List<Place>>
-        get() = _places
+class ReviewViewModel : ViewModel() {
+    private val placesDataSources = PlacesDataSource
+    private lateinit var listener: ListenerRegistration
+
+    private val _reviews by lazy { MutableLiveData<List<Review>>() }
+    val reviews: LiveData<List<Review>>
+        get() = _reviews
 
     private val _error by lazy { MutableLiveData<String>() }
     val error: LiveData<String>
@@ -23,15 +23,12 @@ class PlacesViewModel : ViewModel() {
         _error.value = "Ошибка сервера при обновлении, попробуйте еще раз"
     }
 
-    private val placesDataSources = PlacesDataSource
-    private lateinit var listener: ListenerRegistration
-
-    fun enableListenerPlaces() {
-        val query = PlacesDataSource.getQueryPlaces()
+    fun enableListenerReviews(placeId: String) {
+        val query = PlacesDataSource.getQueryReviews(placeId)
 
         listener = query.addSnapshotListener { value, error ->
             if (value != null) {
-                _places.value = value.toObjects(Place::class.java)
+                _reviews.value = value.toObjects(Review::class.java)
             } else {
                 showError()
             }
