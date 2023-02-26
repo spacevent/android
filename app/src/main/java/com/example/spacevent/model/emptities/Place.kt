121fd3b2f98016ad0fee546e870086e8f8e4508a
4118BarbolinS.P <skat.apps@gmail.com>
2023-02-26 23:04:37 +0300

    Добавление полец

--- app/src/main/java/com/example/spacevent/model/emptities/Place.kt
+++ app/src/main/java/com/example/spacevent/model/emptities/Place.kt
@@ -4,15 +4,16 @@ import java.util.UUID
 
 data class Place(
     val id: String = UUID.randomUUID().toString(),
+    val name: String = "",
     val type: String = "loft",
     val countPeople: Int = 0,
     val city: String = "",
-    val price: Int = 0,
+    val pricies: List<Int> = emptyList(),
+    val rules: List<String> = emptyList(),
     val address: String = "",
     val contacts: String = "",
     val images: List<String> = emptyList(),
     val logo: String = "",
-    val name: String = "",
     val rating: List<Double> = emptyList(),
     val videous: List<String> = emptyList()
 )
