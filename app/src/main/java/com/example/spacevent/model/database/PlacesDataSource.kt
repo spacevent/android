880e776b8c425c05b7b4a83fd8ded5c7876bcf0a
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-19 23:04:02 +0300

    Реализация получения и добавления услуги

--- app/src/main/java/com/example/spacevent/model/database/PlacesDataSource.kt
+++ app/src/main/java/com/example/spacevent/model/database/PlacesDataSource.kt
@@ -1,24 +1,35 @@
 package com.example.spacevent.model.database
 
-import com.example.spacevent.model.emptities.Place
+import com.example.spacevent.model.emptities.ServiceModel
 import com.example.spacevent.model.emptities.Review
 import com.google.android.gms.tasks.Task
 import com.google.firebase.firestore.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 object PlacesDataSource {
     private val db = FirebaseFirestore.getInstance()
 
     fun getQueryPlaces(): Query {
-        return db.collection("places")
+        return db.collection("regions ")
+            .document("Saint-Petersburg")
+            .collection("cities")
+            .document("Saint-Petersburg")
+            .collection("places")
     }
 
     fun getQueryReviews(placeId: String): Query {
         return db.collection("places").document(placeId).collection("reviews")
     }
 
-    suspend fun createPlace(place: Place): Task<Void> {
-        return db.collection("places").document(place.id).set(place)
-    }
+    suspend fun createPlace(serviceModel: ServiceModel): Task<DocumentReference> =
+        withContext(Dispatchers.IO) {
+            return@withContext db.collection("regions ")
+                .document("Saint-Petersburg")
+                .collection("cities")
+                .document("Saint-Petersburg")
+                .collection("places").add(serviceModel)
+        }
 
     suspend fun updatePlace(placeId: String, changes: Map<String, Any>): Task<Void> {
         return db.collection("places").document(placeId).update(changes)
