3847898111f67ef00c93cc59c5f4a34e3dbd3001
4118BarbolinS.P <skat.apps@gmail.com>
2023-03-19 23:05:32 +0300

    Создание ServiceModel.kt

---
+++ app/src/main/java/com/example/spacevent/model/emptities/ServiceModel.kt
@@ -0,0 +1,23 @@
+package com.example.spacevent.model.emptities
+
+import com.google.firebase.firestore.Query
+
+data class ServiceModel(
+        val id: String = "",
+        val name: String = "",
+        val profession: String = "loft",
+        val category: String = "",
+        val amountOfReviews: Int = 0,
+        val overallRating: Int = 0,
+        val numericalParameters: Map<String, Int> = emptyMap<String, Int>(),
+        val workingHours: List<String> = emptyList(),
+        val sellingText: String = "",
+        val characteristics: Map<String, String> = emptyMap<String, String>(),
+        val rules: List<String> = emptyList(),
+        val otherServices: List<Query> = emptyList(),
+        val contacts: Map<String, String> = emptyMap<String, String>(),
+        val address: String = "",
+        val images: List<String> = emptyList(),
+        val videos: List<String> = emptyList()
+)
+data class Item(val name: String = "")
