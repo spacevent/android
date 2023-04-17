23d1bc205af9588ba5e6346d9473fe6bf03c3d3b
4118BarbolinS.P <skat.apps@gmail.com>
2023-04-18 02:12:20 +0300

    Реализация UI

--- app/src/main/java/com/example/spacevent/ui/components/CardInformation.kt
+++ app/src/main/java/com/example/spacevent/ui/components/CardInformation.kt
@@ -11,14 +11,17 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun CardInformationText(
     title: String,
-    items: List<String>
+    items: List<String>,
 ) {
     Box(contentAlignment = Alignment.BottomCenter) {
         Column(
@@ -50,8 +53,50 @@ fun CardInformationText(
                 modifier = Modifier.padding(start = 16.dp, top = 16.dp),
                 color = MaterialTheme.colors.onSurface
             )
+        }
+    }
+}
+
+@Composable
+fun CardInformationText(
+    title: String,
+    items: Map<String, String>,
+) {
+    Box(contentAlignment = Alignment.BottomCenter) {
+        Column(
+            modifier = Modifier
+                .height(180.dp)
+                .padding(horizontal = 32.dp)
+                .clip(RoundedCornerShape(32.dp))
+                .background(MaterialTheme.colors.primaryVariant)
+                .padding(bottom = 8.dp),
+        ) {
+            Text(
+                text = title,
+                color = MaterialTheme.colors.onPrimary,
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.h5,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(MaterialTheme.colors.surface)
+                    .padding(vertical = 8.dp)
+            )
+
+            val text = buildAnnotatedString {
+                items.forEach {
+                    withStyle(SpanStyle(fontWeight = FontWeight.Bold)) {
+                        append("● ${it.key}:")
+                    }
+                    append("${it.value} \n")
+                }
+            }
 
 
+            Text(
+                text = text,
+                modifier = Modifier.padding(start = 16.dp, top = 16.dp),
+                color = MaterialTheme.colors.onSurface
+            )
         }
     }
 }
