f6b1649fe7b190e29d5fd665ca1067e615eaa7f8
barbolin-semyon <skat.apps@gmail.com>
2023-04-18 10:18:13 +0300

    Редактирование внешнего вида

--- app/src/main/java/com/example/spacevent/ui/screens/detailService/RatesScreen.kt
+++ app/src/main/java/com/example/spacevent/ui/screens/detailService/RatesScreen.kt
@@ -1,19 +1,25 @@
 package com.example.spacevent.ui.screens.detailService
 
+import android.util.Log
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.toUpperCase
 import androidx.compose.ui.unit.dp
 import com.example.spacevent.ui.components.CardInformation
 import com.example.spacevent.ui.components.CardInformationText
@@ -28,15 +34,40 @@ fun RatesScreen(serviceId: String, viewModel: ServiceViewModel) {
     if (rates.isNotEmpty()) {
         LazyColumn(
             modifier = Modifier
-                .fillMaxHeight()
-                .background(MaterialTheme.colors.background)
+                .fillMaxSize()
+                .background(MaterialTheme.colors.secondary)
                 .padding(top = 64.dp),
             verticalArrangement = Arrangement.spacedBy(8.dp)
         ) {
             items(rates) {
-                CardInformation(title = it.name) {
-                    Text(it.description)
-                    Text("${it.price} рублей", style = MaterialTheme.typography.h5)
+                Card(
+                    Modifier.padding(horizontal = 16.dp),
+                    backgroundColor = MaterialTheme.colors.surface,
+                    elevation = 16.dp
+                ) {
+                    Column(
+                        verticalArrangement = Arrangement.spacedBy(8.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(text = it.name.uppercase(), style = MaterialTheme.typography.h5)
+
+                        Text(
+                            it.description,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(8.dp),
+                            textAlign = TextAlign.Center
+                        )
+
+                        Text(
+                            "Стоимость: ${it.price} рублей",
+                            fontWeight = FontWeight.Bold,
+                        )
+
+                        Button(onClick = { /*TODO*/ }) {
+                            Text(text = "Написать продавцу", color = Color.White)
+                        }
+                    }
                 }
             }
         }
